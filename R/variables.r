b6271b7e17b63440f0ce42e7cee0d0ac6bf50fdd
Florentin Bulot <f.bulot@soton.ac.uk>
2023-04-07 15:39:56 +0100

    Update variables.r

--- R/variables.r
+++ R/variables.r
@@ -1,4 +1,4 @@
-sensors_raw_file <- paste0(here::here(), "/data/sensors_raw_flagged.Rds")
+sensors_raw_file <- paste0(here::here(), "/data/sensors_raw.Rds")
 ops_file_raw <- paste0(here::here(), "/data/ops.rds")
 ops_file_transformed <- paste0(here::here(), "/data/ops_transformed.rds")
 nanotracer_file <- paste0(here::here(), "/data/nanotracer.rds")
